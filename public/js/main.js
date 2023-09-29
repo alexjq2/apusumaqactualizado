375dc116ee91e3fefaea154df90801a572e087cd
alexjq2 <136828009+alexjq2@users.noreply.github.com>
2023-09-28 20:25:18 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,4 @@
-const ham = document.querySelector('.ham-menu')
-const nav = document.querySelector('.navbar')
 
-ham.addEventListener("click",()=>{
-    ham.classList.toggle('active')
-    nav.classList.toggle('active')
-})
 
 const loadder = document.getElementById('preloadder')
 
